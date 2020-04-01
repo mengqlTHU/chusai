81fa79f24ec1c1213fc3140f0075716f999a6052
mengql16 <mengql16@mails.tsinghua.edu.cn>
2020-04-01 15:30:31 +0800

    add time function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <sstream>
 #include "CircuitFinder.h"
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 
 int main()
 {
+    auto start = system_clock::now();
     CircuitFinder cf;
     cf.loadTestData("../data/test_data.txt");
     cf.run();
+    auto duration = duration_cast<microseconds>(system_clock::now() - start);
+    cout <<  "Cost:"
+         << double(duration.count()) * microseconds::period::num / microseconds::period::den
+         << "Seconds" << endl;
     return 0;
 }
