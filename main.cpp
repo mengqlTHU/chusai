9e0925efff45305ac1e038401d1b73a5af6bc80d
lhb8125 <lhb8125@users.noreply.github.com>
2020-04-10 10:46:18 +0800

    loadTestData
    1. 添加计时函数
    2. 字符串转整数改进
    3. map替换为hashmap

--- main.cpp
+++ main.cpp
@@ -1,16 +1,507 @@
-//#pragma comment(linker, "/STACK:1073741824") //Require 1GB Stack RAM
+#ifndef CIRCUITFINDER_H
+#define CIRCUITFINDER_H
 
+#include <algorithm>
 #include <iostream>
+#include <list>
+#include <vector>
+#include <queue>
+#include <fstream>
 #include <sstream>
-#include "CircuitFinder.h"
+#include <map>
+#include <hash_map>
+#include <set>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
+
+#define MYTIME
+
+#define TEST
+
+// #define TIMERS
+
+#ifdef MYTIME
+#include <sys/time.h>
+#endif
+
+#ifdef TIMERS
+#include "Timers.hpp"
+#endif
+
+typedef std::list<int> NodeList;
+
+class CircuitFinder
+{
+  std::vector<NodeList> AK;
+  std::vector<int> Stack;
+  std::vector<bool> Blocked;
+  std::vector<NodeList> B;
+  // map<int, int> m;
+  vector<int> nodes;
+  vector<vector<vector<int>>> resVect;
+  int N;
+  int circuitCount;
+  int S;
+  time_point<system_clock, nanoseconds> start;
+
+  void unblock(int U);
+  bool circuit(int V);
+  void circuitIterate(int V);
+  void output();
+  int findMin();
+  static bool compareVector(vector<int> v1, vector<int> v2);
+  void outputTime(string info);
+  void sortVector();
+  void printVector(string filename);
+  void printMap();
+  void strongComponent();
+  void removeNode(int s);
+
+public:
+  CircuitFinder()
+      :resVect(5) //3,4,5,6,7
+  {
+      N=0;
+      circuitCount=0;
+      start = system_clock::now();
+  }
+
+  void run();
+  void loadTestData(string filename);
+  void loadTestData_lhb(string filename);
+};
+
+void CircuitFinder::strongComponent()
+{
+    map<int, int> preOrder;
+    map<int, int> lowLink;
+    set<int> sccFound;
+    queue<int> sccQueue;
+    int v;
+    bool done;
+    int i = 0;
+    for (int node = 0; node < N; node++)
+    {
+        queue<int> q;
+        if (!sccFound.count(node))
+            q.push(node);
+
+        while (!q.empty())
+        {
+            v = q.back();
+            if (!preOrder.count(v))
+            {
+                i++;
+                preOrder[v] = i;
+            }
+        }
+        done = true;
+        for (int w : AK[v])
+        {
+            if (!preOrder.count(w))
+            {  
+                q.push(w);
+                done = false;
+                break;
+            }
+        }
+        if (done)
+        {
+            lowLink[v] = preOrder[v];
+            for (int w : AK[v])
+            {
+                if (!sccFound.count(w))
+                {
+                    if (preOrder[w] > preOrder[v])
+                        lowLink[v] = lowLink[v] < lowLink[w] ? lowLink[v] : lowLink[w];
+                    else
+                        lowLink[v] = lowLink[v] < preOrder[w] ? lowLink[v] : preOrder[w];
+                }
+            }
+            q.pop();
+            if (lowLink[v] == preOrder[v])
+            {
+                set<int> scc;
+                scc.insert(v);
+                while (!sccQueue.empty() && preOrder[sccQueue.back()] > preOrder[v])
+                {
+                    int k = sccQueue.back();
+                    sccQueue.pop();
+                    scc.insert(k);
+                }
+                sccFound.insert(scc.begin(), scc.end());
+            }
+            else
+                sccQueue.push(v);
+        }
+    }
+
+}
+
+void CircuitFinder::removeNode(int s)
+{
+    for (int I = s; I < N; ++I)
+    {
+        auto IT = std::find(AK[I].begin(), AK[I].end(), s);
+        if (IT != AK[I].end())
+            AK[I].erase(IT);
+    }
+}
+
+void CircuitFinder::unblock(int U)
+{
+  Blocked[U - 1] = false;
+
+  while (!B[U - 1].empty()) {
+    int W = B[U - 1].front();
+    B[U - 1].pop_front();
+
+    if (Blocked[W - 1]) {
+      unblock(W);
+    }
+  }
+}
+
+void CircuitFinder::loadTestData(string filename)
+{
+    __gnu_cxx::hash_map<int,int> intHash(20000);
+    ifstream indata;
+    indata.open(filename);
+    string line;
+    int vertexIndex = 1;
+    int AK_ptr[280000];
+    while (getline(indata, line)) {
+        char* s = &line[0];
+        int tmp=0;
+        int accountOut=0, accountIn=0;
+        while(*s!=',')
+        {
+            accountOut = (*s++-'0')+accountOut*10;
+        }
+        s++;
+        while(*s!=',')
+        {
+            accountIn = (*s++-'0')+accountIn*10;
+        }
+        if(intHash.find(accountOut)==intHash.end())
+        // if (!m.count(accountOut))
+        {
+            intHash[accountOut] = vertexIndex++;
+            // m[accountOut] = vertexIndex++;
+            nodes.push_back(accountOut);
+            AK.push_back(NodeList());
+            Blocked.push_back(false);
+            B.push_back(NodeList());
+        }
+
+        if(intHash.find(accountIn)==intHash.end())
+        // if (!m.count(accountIn)) // 1700us
+        {
+            // m[accountIn] = vertexIndex++;
+            intHash[accountIn] = vertexIndex++;
+            nodes.push_back(accountIn);
+            AK.push_back(NodeList());
+            Blocked.push_back(false);
+            B.push_back(NodeList());
+        }
+
+        AK[intHash[accountOut] - 1].push_back(intHash[accountIn]); // 400us
+    }
+    N = vertexIndex - 1;
+#ifdef mydebug
+    outputTime("Load Data");
+    printMap();
+#endif
+}
+
+void CircuitFinder::circuitIterate(int V)
+{
+    Stack.push_back(V);
+    Blocked[V - 1] = true;
+    vector<int> s;
+    s.push_back(V);
+    vector<NodeList> sNode;
+    sNode.push_back(AK[V - 1]);
+    set<int> closed;
+    int thisNode, nextNode;
+    NodeList nbrs;
+    while (!s.empty())
+    {
+        thisNode = s.back();
+        nbrs = sNode.back();
+        if (!nbrs.empty() && Stack.size()<8)
+        {
+            nextNode = nbrs.back();
+            sNode.back().pop_back();
+            if (nextNode < V)
+                continue;
+            if (nextNode == V)
+            {
+                output();
+                closed.insert(Stack.begin(), Stack.end());
+            }
+            else if (!Blocked[nextNode - 1])
+            {
+                Stack.push_back(nextNode);
+                s.push_back(nextNode);
+                sNode.push_back(AK[nextNode - 1]);
+                closed.erase(nextNode);
+                Blocked[nextNode - 1] = true;
+                continue;
+            }
+        }
+        if (nbrs.empty() || Stack.size()>7)
+        {
+            if (closed.count(thisNode))
+            {
+                unblock(thisNode);
+            }
+            else
+            {
+                for (int W : AK[thisNode - 1])
+                {
+                    auto IT = std::find(B[W - 1].begin(), B[W - 1].end(), thisNode);
+                    if (IT == B[W - 1].end())
+                        B[W - 1].push_back(thisNode);
+                }
+            }
+            s.pop_back();
+            sNode.pop_back();
+            Stack.pop_back();
+        }
+    }
+}
+
+bool CircuitFinder::circuit(int V)
+{
+  bool F = false;
+
+  Stack.push_back(V);
+  Blocked[V - 1] = true;
+
+  auto circuitLen = Stack.size();
+  if (circuitLen < 8)
+  {
+      for (int W : AK[V - 1]) {
+          if (W == S) {
+              output();
+              F = true;
+          }
+          else if (!Blocked[W - 1]) {
+              if (circuit(W))
+                  F=true;
+          }
+      }
+  }
+  else
+      F = true;
+
+  if (F) {
+    unblock(V);
+  } else {
+    for (int W : AK[V - 1]) {
+      auto IT = std::find(B[W - 1].begin(), B[W - 1].end(), V);
+      if (IT == B[W - 1].end()) {
+        B[W - 1].push_back(V);
+      }
+    }
+  }
+
+  Stack.pop_back();
+  return F;
+}
+
+int CircuitFinder::findMin()
+{
+    int min = nodes[*Stack.begin()-1];
+    int idOfMin = 0;
+    int id = 0;
+    for (auto I = Stack.begin() + 1, E = Stack.end(); I != E; ++I)
+    {
+        id++;
+        if (nodes[*I-1]<min)
+        {
+            min = nodes[*I-1];
+            idOfMin = id;
+        }
+    }
+    return idOfMin;
+}
+
+
+bool CircuitFinder::compareVector(vector<int> v1, vector<int> v2)
+{
+    for (int i=0;i<v1.size();i++)
+    {
+        if (v1[i]!=v2[i])
+            return v1[i]<v2[i];
+    }
+    return true;
+}
+
+void CircuitFinder::outputTime(string info)
+{
+    auto duration = duration_cast<microseconds>(system_clock::now() - start);
+    cout <<  info
+         << double(duration.count()) * microseconds::period::num / microseconds::period::den
+         << "Seconds" << endl;
+}
+
+void CircuitFinder::output()
+{
+  auto circuitLen = Stack.size();
+  if (circuitLen>2)
+  {
+      resVect[circuitLen-3].push_back(vector<int>());
+      int idOfMin = findMin();
+//      std::cout << "circuit: ";
+      for (int i=idOfMin;i<circuitLen+idOfMin;i++)
+      {
+          auto I = Stack.begin()+(i % circuitLen);
+//          std::cout << nodes[*I-1] << " -> ";
+          resVect[circuitLen-3].back().push_back(nodes[*I-1]);
+      }
+//      std::cout << nodes[*(Stack.begin()+idOfMin)-1] << std::endl;
+      circuitCount += 1;
+  }
+}
+
+void CircuitFinder::sortVector()
+{
+    for (int i=0;i<5;i++)
+    {
+        if (resVect[i].size()>0)
+        {
+            sort(resVect[i].begin(),resVect[i].end(), compareVector);
+        }
+    }
+}
+
+void CircuitFinder::printVector(string filename)
+{
+    ofstream fout(filename);
+    fout << circuitCount << endl;
+    for (int i=0;i<5;i++)
+    {
+        for (int j=0;j<resVect[i].size();j++)
+        {
+            fout << resVect[i][j][0];
+            for (int k=1;k<i+3;k++)
+                fout << "," << resVect[i][j][k];
+            fout << endl;
+        }
+    }
+    fout.close();
+}
+
+void CircuitFinder::printMap()
+{
+    ofstream fout("../data/map.txt");
+    for (int i = 0; i < N; i++)
+        fout << nodes[i] << endl;
+    fout.close();
+}
+
+void CircuitFinder::run()
+{
+  Stack.clear();
+  S = 1;
+#ifdef MYTIME
+    struct timeval ov_start, ov_end;
+    gettimeofday(&ov_start,NULL);
+#endif
+  while (S < N) {
+    for (int I = S; I <= N; ++I) {
+      Blocked[I-1] = false;
+      B[I-1].clear();
+    }
+    circuit(S);
+    //circuitIterate(S);
+
+    removeNode(S);
+
+    ++S;
+
+#ifdef mydebug
+    outputTime("A S cycle");
+    cout << S << endl;
+#endif
+  }
+#ifdef MYTIME
+    gettimeofday(&ov_end,NULL);
+    double timeuse
+        = 1000000*(ov_end.tv_sec-ov_start.tv_sec)
+        + ov_end.tv_usec-ov_start.tv_usec;
+    cout<<"run_circuit: "<<timeuse<<" us"<<endl;
+#endif
+
+#ifdef mydebug
+  printVector("../data/myresult_unsorted.txt");
+#endif
+
+#ifdef MYTIME
+    gettimeofday(&ov_start,NULL);
+#endif
+  sortVector();
+#ifdef MYTIME
+    gettimeofday(&ov_end,NULL);
+    timeuse
+        = 1000000*(ov_end.tv_sec-ov_start.tv_sec)
+        + ov_end.tv_usec-ov_start.tv_usec;
+    cout<<"sortVector: "<<timeuse<<" us"<<endl;
+#endif
+#ifdef MYTIME
+    gettimeofday(&ov_start,NULL);
+#endif
+  printVector("/projects/student/result.txt");
+#ifdef MYTIME
+    gettimeofday(&ov_end,NULL);
+    timeuse
+        = 1000000*(ov_end.tv_sec-ov_start.tv_sec)
+        + ov_end.tv_usec-ov_start.tv_usec;
+    cout<<"printVector: "<<timeuse<<" us"<<endl;
+#endif
+  outputTime("Finished");
+}
+
+#endif // CIRCUITFINDER_H
 
 
 int main()
 {
+#ifdef TIMERS
+    MPI_Init(NULL,NULL);
+    Timer:startTimer("overall");
+#endif
     CircuitFinder cf;
-    cf.loadTestData("../data/test_data.txt");
+#ifdef MYTIME
+    struct timeval ov_start, ov_end;
+    gettimeofday(&ov_start,NULL);
+#endif
+#ifdef TEST
+    cf.loadTestData("/root/data/test_data_small.txt");
+#else
+    cf.loadTestData("/data/test_data.txt");
+#endif
+#ifdef MYTIME
+    gettimeofday(&ov_end,NULL);
+    double timeuse
+        = 1000000*(ov_end.tv_sec-ov_start.tv_sec)
+        + ov_end.tv_usec-ov_start.tv_usec;
+    cout<<"loadTestData: "<<timeuse<<" us"<<endl;
+#endif
+#ifdef MYTIME
+    gettimeofday(&ov_start,NULL);
+#endif
     cf.run();
+#ifdef MYTIME
+    gettimeofday(&ov_end,NULL);
+    timeuse
+        = 1000000*(ov_end.tv_sec-ov_start.tv_sec)
+        + ov_end.tv_usec-ov_start.tv_usec;
+    cout<<"run: "<<timeuse<<" us"<<endl;
+#endif
     return 0;
 }
